a60b9b1d09cb09a9fa1ccb86d765fe9a03bd14bd
Doina Plesca <doinitzaplesca@yahoo.com>
2025-11-18 16:32:25 +0100

    add evaluator to checks if yogentâ  output is correct

---
+++ project/evaluate.py
@@ -0,0 +1,28 @@
+from autogen import AssistantAgent, UserProxyAgent
+from config import LLM_CONFIG
+
+# create an evaluation assistant to read the task, read the agent’s output, say corect or incorect
+eval_agent = AssistantAgent(
+    name="eval_agent",
+    llm_config=LLM_CONFIG,
+    system_message="You evaluate whether the agent completed the task correctly. Reply briefly."
+)
+
+# create a user proxy to automate conversation(no human types anything,everything runs automatically
+user = UserProxyAgent(
+    name="student",
+    human_input_mode="NEVER"
+)
+
+# checks if agent did its job correctly
+def evaluate(task_description: str, agent_output: str):
+    msg = f"""
+Task:
+{task_description}
+
+Agent Output:
+{agent_output}
+
+Did the agent complete the task correctly?
+"""
+    return user.initiate_chat(eval_agent, message=msg)
